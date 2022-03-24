7271d8e30b862c83a29a309e02fefcc70cbabaea
sandoramix <sandoramixer61@gmail.com>
2022-03-24 06:16:06 +0100

    full responsiveness, addes sidebar with user list and theme switch button

--- client/listeners.js
+++ client/listeners.js
@@ -101,3 +101,15 @@ $(() => {
 		_top = _cur_top;
 	});
 });
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty(`--vh`, `${vh}px`);
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty(`--vw`, `${vw}px`);
+
+window.addEventListener(`resize`, () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty(`--vh`, `${vh}px`);
+	let vw = window.innerWidth * 0.01;
+	document.documentElement.style.setProperty(`--vw`, `${vw}px`);
+});
